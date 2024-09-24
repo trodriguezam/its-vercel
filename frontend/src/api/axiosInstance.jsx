d9144b3270b2bd406a4df8362614ced30da7fa8d
trodriguezam <trodriguez4@miuandes.cl>
2024-09-24 18:10:06 -0300

    feat: implemented backend url

--- frontend/src/api/axiosInstance.jsx
+++ frontend/src/api/axiosInstance.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://its-backend-076f2e73fcd4.herokuapp.com/api',
 });
 
 export default axiosInstance;
