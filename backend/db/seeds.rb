9ae265c6946876dcff44d2384103eb29a5d2d3a0
trodriguezam <trodriguez4@miuandes.cl>
2024-09-24 20:44:46 -0300

    repo fix

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,20 +1,23 @@
 Answer.destroy_all
+UserQuestion.destroy_all
 Question.destroy_all
 Task.destroy_all
 Topic.destroy_all
 
 topic = Topic.create(name: "Estática de sólidos rígidos")
 
-task = Task.create(name: "Tipos de Fuerzas", topic_id: topic.id, task_type: "Option", difficulty: 1)
+task1 = Task.create(name: "Tipos de Fuerzas", topic_id: topic.id, task_type: "Option", difficulty: 1)
+task2 = Task.create(name: "DCL Simple", topic_id: topic.id, task_type: "Development", difficulty: 1)
+task3 = Task.create(name: "DCL Complejo", topic_id: topic.id, task_type: "Development", difficulty: 2)
 
-question1 = Question.create(task_id: task.id, question_text: "¿Cuál de las siguientes fuerzas es una fuerza de contacto?")
+question1 = Question.create(task_id: task1.id, question_text: "¿Cuál de las siguientes fuerzas es una fuerza de contacto?")
 
 Answer.create(question_id: question1.id, answer_text: "Fuerza de gravedad", correct: false)
 Answer.create(question_id: question1.id, answer_text: "Fuerza normal", correct: true)
 Answer.create(question_id: question1.id, answer_text: "Fuerza electroestática", correct: false)
 Answer.create(question_id: question1.id, answer_text: "Fuerza de tensión", correct: false)
 
-question2 = Question.create(task_id: task.id, question_text: "¿Cuál de las siguientes fuerzas es una fuerza de campo?")
+question2 = Question.create(task_id: task1.id, question_text: "¿Cuál de las siguientes fuerzas es una fuerza de campo?")
 
 Answer.create(question_id: question2.id, answer_text: "Fuerza de gravedad", correct: true)
 Answer.create(question_id: question2.id, answer_text: "Fuerza normal", correct: false)
