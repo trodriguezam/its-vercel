29ceaad7d7a71d2d941c1c59f7cb4c8a169bc8c1
trodriguezam <trodriguez4@miuandes.cl>
2024-09-26 04:09:29 -0300

    porfavor

--- frontend/src/api/axiosInstance.jsx
+++ frontend/src/api/axiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:3000/api',
-  baseURL: 'https://its-backend-076f2e73fcd4.herokuapp.com/api',
+  baseURL: 'https://its-backend-bb7981776b07.herokuapp.com/api',
 });
 
 export default axiosInstance;
