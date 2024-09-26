3603249e3ad262cbfdee7f4e5aa880b655a2bf7f
trodriguezam <trodriguez4@miuandes.cl>
2024-09-26 03:13:54 -0300

    cors?

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://its-vercel.vercel.app'  # Allow your Vercel frontend
+    # origins 'https://its-vercel.vercel.app'  # Allow your Vercel frontend
+    origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
